e557883f26876da3b90bd334975d8300577a7081
Iakov Lilo <jlilo@hwdtech.com>
2022-04-01 11:04:40 +0600

    Add project files.

---
+++ OpenSilverXamlBinding/OpenSilverXamlBinding/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace OpenSilverXamlBinding
+{
+    public partial class MainPage : Page
+    {
+        public string CustomText { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            CustomText = "This text is set via Binding";
+
+            DataContext = this;
+
+            // Enter construction logic here...
+        }
+    }
+}
