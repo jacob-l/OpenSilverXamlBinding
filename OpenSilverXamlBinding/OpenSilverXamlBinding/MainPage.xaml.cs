2d4a8c25cd0fda076845e7b1f64aa641c09cbe83
Iakov Lilo <jlilo@hwdtech.com>
2022-04-19 17:21:06 +0600

    Make the example more explanatory

--- OpenSilverXamlBinding/OpenSilverXamlBinding/MainPage.xaml.cs
+++ OpenSilverXamlBinding/OpenSilverXamlBinding/MainPage.xaml.cs
@@ -1,20 +1,49 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace OpenSilverXamlBinding
 {
+    public class Model : INotifyPropertyChanged
+    {
+        private string _customText = "";
+
+        public string CustomText
+        {
+            get => _customText;
+            set
+            {
+                _customText = value;
+                OnPropertyChanged("CustomText");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
     public partial class MainPage : Page
     {
-        public string CustomText { get; set; }
+        private readonly Model _model = new Model
+        {
+            CustomText = "This text is set via Binding"
+        };
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            CustomText = "This text is set via Binding";
-
-            DataContext = this;
+            DataContext = _model;
 
             // Enter construction logic here...
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _model.CustomText = "Text Changed";
         }
     }
 }
